67c50cdd66c509dce8130acd51541c84478e3802
Shane Hofstetter <shane.hofstetter@gmail.com>
2023-01-24 23:06:46 +0100

    dont capitalize first letter

--- lib/youtrack/resources/base.rb
+++ lib/youtrack/resources/base.rb
@@ -34,7 +34,7 @@ module Youtrack
 
       def prepare_payload(data)
         if data.respond_to?(:deep_transform_keys)
-          data = data.deep_transform_keys(&:camelize)
+          data = data.deep_transform_keys { |k| k.camelize(:lower) }
         end
         data.respond_to?(:to_json) ? data.to_json : data
       end
